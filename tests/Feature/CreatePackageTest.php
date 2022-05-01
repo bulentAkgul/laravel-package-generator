74b2aceeb2dbf03a120ac55c4d7feb53560804d0
Bulent Akgul <bulent.akgul@me.com>
2022-05-01 10:06:23 +0300

    standalone declarations fixed

--- tests/Feature/CreatePackageTest.php
+++ tests/Feature/CreatePackageTest.php
@@ -6,9 +6,11 @@ use Bakgul\Kernel\Tests\Concerns\HasTestMethods;
 use Bakgul\Kernel\Helpers\Folder;
 use Bakgul\Kernel\Helpers\Path;
 use Bakgul\Kernel\Helpers\Settings;
+use Bakgul\Kernel\Tests\Services\TestDataService;
 use Bakgul\Kernel\Tests\Tasks\SetupTest;
 use Bakgul\Kernel\Tests\TestCase;
 use Illuminate\Support\Str;
+
 class CreatePackageTest extends TestCase
 {
     use HasTestMethods;
@@ -16,7 +18,7 @@ class CreatePackageTest extends TestCase
     /** @test */
     public function package_will_not_be_created_when_standalone_laravel_is_true()
     {
-        $this->testPackage = (new SetupTest)([false, true]);
+        $this->testPackage = (new SetupTest)(TestDataService::standalone('sl'));
 
         $this->artisan('create:package users core');
 
@@ -27,7 +29,7 @@ class CreatePackageTest extends TestCase
     /** @test */
     public function package_will_be_created_on_the_base_when_standalone_package_is_true()
     {
-        $this->testPackage = (new SetupTest)([true, false]);
+        $this->testPackage = (new SetupTest)(TestDataService::standalone('sp'));
 
         config()->set('packagify.identity.package', 'my test package');
         config()->set('packagify.identity.registrar', 'register-with-me');
@@ -52,7 +54,7 @@ class CreatePackageTest extends TestCase
     /** @test */
     public function package_will_be_created_on_the_root_when_standalone_is_false()
     {
-        $this->testPackage = (new SetupTest)([false, false], true);
+        $this->testPackage = (new SetupTest)(TestDataService::standalone('pl'), true);
 
         $this->artisan("create:package {$this->testPackage['name']} {$this->testPackage['folder']}");
 
