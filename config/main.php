fe44d22b885b856aa061d99228f52a8ce9a675cf
Bulent Akgul <bulent.akgul@me.com>
2022-05-05 22:25:10 +0300

    comment block added

--- config/main.php
+++ config/main.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Main From Package Generator
+    |--------------------------------------------------------------------------
+    |
+    | When your repository is a packagified laravel, the following value
+    | will be the folder where all package are located.
+    |
+    */
     'packages_root' => 'packages',
 ];
--- config/structures.php
+++ config/structures.php
@@ -1,6 +1,26 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | The Skeleton To Generate Packages
+    |--------------------------------------------------------------------------
+    |
+    | The following structure will be used when a package is being generated.
+    | 
+    | FILE : The list of files that will be created in that root. The keys of
+    |        FILE arrays are the stub names, and values are the file names.
+    | Other keys are the folders' names. When their values are empty array,
+    | we'll have empty directories.
+    |
+    | {{ ... }} are placeholders whose values come from the settings or code.
+    |
+    | When you create a standalone package, some files will be copied from
+    | the vendor folder.
+    |
+    | You can change this structure as you need.
+    |
+    */
     'package' => [
         'FILES' => ['composer' => 'composer.json', 'readme' => 'README.md'],
         'config' => ['FILES' => ['config' => '{{ registrar }}.php']],
