22ea826e923b93e56d2ef17c806029bef753a389
Bulent Akgul <bulent.akgul@me.com>
2022-05-15 12:22:26 +0300

    help content added

--- src/Commands/CreatePackageCommand.php
+++ src/Commands/CreatePackageCommand.php
@@ -17,7 +17,27 @@ class CreatePackageCommand extends Command
     use HasHistory, HasPreparation, HasRequest, Sharable, ShouldBeEvaluated;
 
     protected $signature = 'create:package {package?} {root?} {--D|dev}';
-    protected $description = '';
+
+    protected $description = 'This command will generate a package if you work on a Packagefied Laravel repository.';
+
+    protected $arguments = [
+        'package' => [
+            "Require",
+            "The name of the package must be unique throughout the repository."
+        ],
+        'root' => [
+            "Require",
+            "It must be one of the defined roots in the 'roots' array on *config/packagify.php*"
+        ],
+    ];
+    protected $options = [
+        'dev' => [
+            "If you create a dev-dependency, add '-d' or '--dev' to the command. "
+        ]
+    ];
+
+    protected $examples = [];
+
 
     public function __construct()
     {
@@ -28,6 +48,10 @@ class CreatePackageCommand extends Command
 
     public function handle()
     {
+        if (Settings::standalone('laravel')) $this->write('Laravel');
+        
+        if (Settings::standalone('package') && $this->argument('name')) $this->write('Package');
+
         $this->prepareRequest();
 
         if (Settings::evaluator('evaluate_commands')) {
@@ -38,5 +62,10 @@ class CreatePackageCommand extends Command
         $this->logFile();
 
         (new PackageService)->handle($this->makePackageRequest());
+    }
+
+    private function write($type)
+    {
+        $this->error("This command can't be executed when the repository is s Standalone {$type}");
     }
 }
