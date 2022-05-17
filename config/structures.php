2d0a93041419fbdd1af2099c8ce9ff6cc8de67bf
Bulent Akgul <bulentakgul1980@gmail.com>
2022-05-17 11:27:15 +0300

    config comments improved

--- config/structures.php
+++ config/structures.php
@@ -8,18 +8,21 @@ return [
     |
     | The following structure will be used when a package is being generated.
     | 
-    | FILE : The list of files that will be created in that root. The keys of
-    |        FILE arrays are the stub names, and values are the file names.
-    | Other keys are the folders' names. When their values are empty array,
-    | we'll have empty directories.
-    |
-    | {{ ... }} are placeholders whose values come from the settings or code.
-    |
-    | When you create a standalone package, some files will be copied from
-    | the vendor folder.
-    |
+    | FILES: The list of files that will be created in that root.
+    |        The keys of that arrays are the stub names, and values 
+    |        are the file names.
+    | 
+    | Other keys are the folders' names. When their values are empty
+    | array, we'll have empty directories.
+    | 
+    | {{ ... }} are placeholders whose values come from the settings
+    | or code.
+    | 
+    | When you create a standalone package, some files will be
+    | copied from the vendor folder.
+    | 
     | You can change this structure as you need.
-    |
+    | 
     */
     'package' => [
         'FILES' => ['composer' => 'composer.json', 'readme' => 'README.md'],
