7d4d88a0c0eee9cfe756ec2d600a888ef2e127ca
Bulent Akgul <bulentakgul1980@gmail.com>
2022-05-26 22:50:01 +0300

    api middleware added to api routes

--- files/routes/api.php
+++ files/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
-    Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware(['api', 'auth:sanctum'])->group(function () {
         Route::apiResources([]);
     });
 });
