34ae046a4f7f2b82442acc90349352a0982e6da8
Bulent Akgul <bulent.akgul@me.com>
2022-05-29 10:42:23 +0300

    refactoring

--- src/Tasks/AddApiRoutes.php
+++ src/Tasks/AddApiRoutes.php
@@ -8,30 +8,38 @@ use Bakgul\Kernel\Helpers\Text;
 
 class AddApiRoutes
 {
-    private static $skeleton = [
-        'Route::prefix(' . "'api{{ app }}'" . ')->group(function () {',
-        '    Route::middleware([' . "'api', 'auth{{ auth }}'" . '])->group(function () {',
-        '        Route::apiResources([]);',
-        '    });',
-        '});'
-    ];
-
     public static function _(array $request)
     {
         $path = "{$request['attr']['path']}/routes/api.php";
 
         $content = file($path);
 
-        foreach (Settings::apps() as $app) {
-
-            $request['map']['app'] = "/{$app['folder']}";
+        foreach (Settings::apps() as $type => $app) {
+            $request = self::extendRequest($request, [...$app, 'type' => $type]);
 
             foreach (self::$skeleton as $line) {
                 $content[] = Text::replaceByMap($request['map'], $line);
             }
+
             $content[] = '';
         }
 
         Content::write($path, $content);
     }
+
+    private static function extendRequest($request, $app)
+    {
+        $request['map']['app'] = "/{$app['folder']}";
+        $request['map']['admin_mv'] = $app['type'] == 'admin' ? Text::wrap('admin', 'sq') . ', ' : '';
+
+        return $request;
+    }
+
+    private static $skeleton = [
+        'Route::prefix(' . "'api{{ app }}'" . ')->group(function () {',
+        '    Route::middleware([' . "{{admin_mv}}'api', 'auth{{ auth }}'" . '])->group(function () {',
+        '        Route::apiResources([]);',
+        '    });',
+        '});'
+    ];
 }
